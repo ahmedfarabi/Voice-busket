0266eef424816196f2b55bf20520ce4b7ee377b3
Ahmed Farabi <farabi2051@gmail.com>
2016-06-02 13:54:53 +0600

    Add files via upload

---
+++ VoiceBasket/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace VoiceBasket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(366, 163);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Tag = "";
+            this.richTextBox1.Text = "~Log~";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(397, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enable Voice Command";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(397, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 68);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Disable Voice Command";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 203);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(54, 31);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "^";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(203, 240);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 31);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "<";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(265, 240);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(54, 31);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "v";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(325, 240);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(52, 31);
+            this.button6.TabIndex = 6;
+            this.button6.Text = ">";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 241);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(65, 27);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "HELP";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 269);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(65, 26);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "About";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(447, 269);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(61, 26);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "EXIT";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(204, 275);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(173, 24);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Stop";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 307);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ VoiceBasket/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO.Ports;
+//using namespace std;
+
+namespace VoiceBasket
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.PortName = "COM3";
+            serialPort1.BaudRate = 9600;
+            try
+            {
+                if(!serialPort1.IsOpen)
+                serialPort1.Open();
+            }
+            catch (Exception ex)
+            {               MessageBox.Show(ex.Message);
+            }
+            serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(DataReceived);
+        }
+
+        private void DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                SerialPort spl = (SerialPort)sender;
+                Console.Write("Data" + " " + spl.ReadLine() + "\n");
+                //count++;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            button2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("fwd");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("left");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("bwd");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("right");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("s");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("\n Still Working, :( So its nothing to help here.. ");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("\n  No About. :(");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] { "hello", "who am i", "Go","forward", "left", "right", "stop", "back","danee","bamee","strip" });
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammar = new Grammar(gBuilder);
+
+            recEngine.LoadGrammarAsync(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+        }
+
+        void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch(e.Result.Text)
+            {
+                case "hello":
+                    synthesizer.SpeakAsync("hello! is it me you looking for?");
+                    MessageBox.Show("Hello, How are you? What's you doing?");
+                    
+                    break;
+                case"who am i":
+                    synthesizer.SpeakAsync("you are ankur");
+                    break;
+                case "Go":
+                    serialPort1.Write("fwd");
+                    synthesizer.SpeakAsync("forward");
+                    richTextBox1.Text += "\n Forward";
+                    break;
+                case "left":
+                    synthesizer.SpeakAsync("left");
+                    serialPort1.Write("left");
+                    richTextBox1.Text += "\n Left";
+                    break;
+                case "right":
+                    synthesizer.SpeakAsync("right");
+                    serialPort1.Write("right");
+                    richTextBox1.Text += "\n Right";
+                    break;
+                case "back":
+                    synthesizer.SpeakAsync("fall back");
+                    serialPort1.Write("bwd");
+                    richTextBox1.Text += "\n back";
+                    break;
+                case "stop":
+                    synthesizer.SpeakAsync("no motion");
+                    serialPort1.Write("s");
+                    richTextBox1.Text += "\n car stop";
+                    break;
+                
+
+            }
+            //throw new NotImplementedException();
+        }
+    }
+}
